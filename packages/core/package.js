65dceaf2236e089f53f77d114937362f38a57304
Mitar <mitar.git@tnode.com>
2016-08-03 17:34:46 -0700

    Initial version.

---
+++ packages/core/package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'core',
+  version: '0.1.0'
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('1.4.0.1');
+
+  api.use([
+    'coffeescript',
+  ]);
+
+  api.addFiles([
+    'test.coffee'
+  ], 'client');
+});
